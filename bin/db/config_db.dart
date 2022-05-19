b3a95b6154c24f4c31a32b6a8e4a3fc854b8ccb2
Germain Kataku <katakugermain@gmail.com>
2022-05-19 22:04:51 +0100

    ficed  service done

--- bin/db/config_db.dart
+++ bin/db/config_db.dart
@@ -2,7 +2,7 @@ import 'package:postgres/postgres.dart';
 
 class ConnexionDatabase {
   Future<PostgreSQLConnection> connection() async {
-    var connection = PostgreSQLConnection('db', 5432, "postgres",
+    var connection = PostgreSQLConnection("db", 5432, "postgres",
         username: "postgres", password: "kataku", useSSL: false);
 
     print('Database connecting...');
