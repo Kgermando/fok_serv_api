e4bf6de6cd91ad12b687b8b6d2c764b3b5c5a7b4
Germain Kataku <katakugermain@gmail.com>
2022-07-11 16:32:27 +0100

    sssssaqww

--- bin/models/comptabilites/courbe_journal_model.dart
+++ bin/models/comptabilites/courbe_journal_model.dart
@@ -1,5 +1,5 @@
 class CourbeJournalModel {
-  final double created;
+  final DateTime created;
   final double sumDebit;
   final double sumCredit; 
 
@@ -16,7 +16,7 @@ class CourbeJournalModel {
 
   factory CourbeJournalModel.fromJson(Map<String, dynamic> json) {
     return CourbeJournalModel(
-      created: json['created'],
+      created: DateTime.parse(json['created']),
       sumDebit: json['sumDebit'],
       sumCredit: json['sumCredit']
     );
@@ -24,7 +24,7 @@ class CourbeJournalModel {
 
   Map<String, dynamic> toJson() {
     return {
-      'created': created, 
+      'created': created.toIso8601String(), 
       'sumDebit': sumDebit, 
       'sumCredit': sumCredit
     };
