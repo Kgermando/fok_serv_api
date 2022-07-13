fd891bafba4293b2c896142035be020a25c7e0c7
Germain Kataku <katakugermain@gmail.com>
2022-07-13 13:42:14 +0100

    moyen_payement

--- bin/repository/administration/actionnaire_cotisation_repository.dart
+++ bin/repository/administration/actionnaire_cotisation_repository.dart
@@ -23,7 +23,7 @@ class ActionnaireCotisationRepository {
     await executor.transaction((ctx) async {
       await ctx.query(
         "INSERT INTO $tableName (id, reference, nom, post_nom, prenom,"
-        "matricule, note, moyenPayement, numeroTransaction, signature, created)"
+        "matricule, note, moyen_payement, numero_transaction, signature, created)"
         "VALUES (nextval('actionnaire_cotisations_id_seq'), @1, @2, @3, @4, @5, @6, @7 , @8, @9, @10)",
           substitutionValues: {
             '1': data.reference,
@@ -43,7 +43,7 @@ class ActionnaireCotisationRepository {
   Future<void> update(ActionnaireCotisationModel data) async {
     await executor.query("""UPDATE $tableName
       SET reference = @1, nom = @2, post_nom = @3, prenom = @4, matricule = @5, 
-        note = @6, moyenPayement = @7, numeroTransaction = @8,
+        note = @6, moyen_payement = @7, numero_transaction = @8,
         signature = @9, created = @10 WHERE id = @11""", substitutionValues: {
       '1': data.reference,
       '2': data.nom,
